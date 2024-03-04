3741c051994b3246089659333391f3b946e078e2
tienmynguyen <mytom2401@gmail.com>
2024-03-04 21:15:59 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,199 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "0-0",
+        artist: "",
+        img: "music-1",
+        src: "a0"
+    },
+    {
+        name: "01-1",
+        artist: "",
+        img: "music-1",
+        src: "a1"
+    },
+    {
+        name: "01-02",
+        artist: "",
+        img: "music-1",
+        src: "a2"
+    },
+    {
+        name: "01-3",
+        artist: "",
+        img: "music-1",
+        src: "a3"
+    },
+    {
+        name: "02-1",
+        artist: "",
+        img: "music-1",
+        src: "a4"
+    },
+    {
+        name: "02-2",
+        artist: "",
+        img: "music-1",
+        src: "a5"
+    },
+    {
+        name: "02-3",
+        artist: "",
+        img: "music-1",
+        src: "a6"
+    },
+    {
+        name: "03-1",
+        artist: "",
+        img: "music-1",
+        src: "a7"
+    },
+    {
+        name: "03-2",
+        artist: "",
+        img: "music-1",
+        src: "a8"
+    },
+    {
+        name: "03-3",
+        artist: "",
+        img: "music-1",
+        src: "a9"
+    },
+    {
+        name: "03-4",
+        artist: "",
+        img: "music-1",
+        src: "a10"
+    },
+    {
+        name: "04-1",
+        artist: "",
+        img: "music-1",
+        src: "a11"
+    },
+    {
+        name: "04-2",
+        artist: "",
+        img: "music-1",
+        src: "a12"
+    },
+    {
+        name: "04-3",
+        artist: "",
+        img: "music-1",
+        src: "a13"
+    },
+    {
+        name: "05-1",
+        artist: "",
+        img: "music-1",
+        src: "a14"
+    },
+    {
+        name: "05-2",
+        artist: "",
+        img: "music-1",
+        src: "a15"
+    },
+    {
+        name: "06-1",
+        artist: "",
+        img: "music-1",
+        src: "a16"
+    },
+    {
+        name: "06-2",
+        artist: "",
+        img: "music-1",
+        src: "a17"
+    },
+    {
+        name: "06-3",
+        artist: "",
+        img: "music-1",
+        src: "a18"
+    },
+    {
+        name: "07-1",
+        artist: "",
+        img: "music-1",
+        src: "a19"
+    },
+    {
+        name: "07-2",
+        artist: "",
+        img: "music-1",
+        src: "a20"
+    },
+    {
+        name: "07-3",
+        artist: "",
+        img: "music-1",
+        src: "a21"
+    },
+    {
+        name: "08-1",
+        artist: "",
+        img: "music-1",
+        src: "a22"
+    },
+    {
+        name: "08-2",
+        artist: "",
+        img: "music-1",
+        src: "a23"
+    },
+    {
+        name: "08-3",
+        artist: "",
+        img: "music-1",
+        src: "a24"
+    },
+    {
+        name: "09-1",
+        artist: "",
+        img: "music-1",
+        src: "a25"
+    },
+    {
+        name: "09-2",
+        artist: "",
+        img: "music-1",
+        src: "a26"
+    },
+    {
+        name: "09-3",
+        artist: "",
+        img: "music-1",
+        src: "a27"
+    },
+    {
+        name: "10-1",
+        artist: "",
+        img: "music-1",
+        src: "a28"
+    },
+    {
+        name: "10-2",
+        artist: "",
+        img: "music-1",
+        src: "a29"
+    },
+    {
+        name: "10-3",
+        artist: "",
+        img: "music-1",
+        src: "a30"
+    }
+
+];
